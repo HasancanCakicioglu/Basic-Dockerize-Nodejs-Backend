ea107f88f176791d92e29510ace010d0f6d66c92
IstakozNecmi <hasanlol2002@gmail.com>
2023-02-21 13:15:19 +0300

    Basic-Nodejs-Backend

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 
 const connectDB = async () => {
     try {
         //run().catch(console.dir);
-        mongoose.connect("mongodb+srv://testUser:123456test@cluster0.vk6odsj.mongodb.net/test?retryWrites=true&w=majority" ,{ useNewUrlParser: true, useUnifiedTopology: true })
+        mongoose.connect(process.env.MONGO_URL ,{ useNewUrlParser: true, useUnifiedTopology: true })
     } catch (error) {
         console.log(error)
      process.exit(1)
     }
 }
-
-
-
-
-
 
 
 module.exports = { connectDB}
